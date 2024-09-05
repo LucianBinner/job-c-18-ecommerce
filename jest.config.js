dfb95832383399eb54994eab8d33f61ff11c0ec4
Lucian Binner <lucian.binner@hotmail.com>
2024-09-05 14:24:21 -0300

    chore: create jest configuration

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!**/test/**',
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
+}
